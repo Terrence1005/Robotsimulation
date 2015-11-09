ea6892f7b571db8f4a66f5fe8ccc3c8a94f50c8c
Xiaotian Zhang <xiaotiannj@gmail.com>
2015-11-09 14:32:10 -0500

    Update Robotsimulation.h

--- Robotsimulation.h
+++ Robotsimulation.h
@@ -1,9 +1,33 @@
-#ifndef _ROBOT_HH__
-#define _ROBOT_HH__
-
+#ifndef _ROBOT_HH_
+#define _ROBOT_HH_
+#include <iostream>
 class Robot{
-  private:
-  public:
-};
+private:
+    double speed;//robot moving speed;
+    int coordinate_x;
+    int coordinate_y;//location in the coordinate x,y;
+    double theta;//set north as degree 0;
+    double robotsize_radius;//set robot as a circle;
+public:
+    static int count_robots;
+    Robot(double s,int x,int y,double th,double r);
+    ~Robot();
+    void robot_move(int& x,int& y,double& s,double& th);
+    //move the robot at that speed in that direction;
+    double getSpeed(){return speed;}
+    double change_speed(double s);
+    int getX(){return coordinate_x;}
+    int getY(){return coordinate_y;}
+    void change_location(int& x,int& y);
+    //change the robot location;
+    double getTheta(){return theta;}
+    //get the robot direction
+    double change_theta(double th);
+    //change the robot direction
+    int getCount(){return count_robots;}
+    //count the robot numbers
+    double robot_test_range(double r);
+    //let the robot get a test of blocks range in the radius r
 
+};
 #endif
