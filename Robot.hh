1354b1b673b815d187ce87f8c114ea7ccdadeec9
Bacteriaphage <tohyzhang@hotmail.com>
2015-12-01 12:17:52 -0500

    Update Robot.hh

--- Robot.hh
+++ Robot.hh
@@ -1,10 +1,22 @@
+//****************************************************************************************
+//Author: Hanyu Zhang
+//Discription: Define class Robot
+//****************************************************************************************
+
+
 #ifndef ROBOT_HH
 #define ROBOT_HH
 #include <vector>
 
 using namespace std;
 
+//The class Robot can store basic parameters of a robot such as speed, direction, location, etc.
+//And it defines some funtions which can prevent robots from collision. 
+
 class Robot;
+
+//The structure Telecom will record the information of other robots which come into the alert 
+//area of this robot and caculate the distance between this robot to them. 
 
 struct Telecom {
     int    number;
@@ -14,16 +26,16 @@ struct Telecom {
 
 class Robot{
 private:
-    int ID;
+    int ID;                                                          //The ID of each robot
     double speed;                                                    //robot moving speed;
     double coordinate_x;
     double coordinate_y;                                             //location in the coordinate x,y;
-    double theta;                                                 //set north as degree 0;
-    double robotsize_radius;                                      //set robot as a circle;
-    double alert;
-    vector<Telecom> communication;
-    vector<double> trace;
-    static int count_robots;                                      //How many robots in this place;
+    double theta;                                                    //set north as degree 0;
+    double robotsize_radius;                                         //set robot as a circle;
+    double alert;                                                    //set the alert area;
+    vector<Telecom> communication;                                   //A vector to record Telecom structure;
+    vector<double> trace;                                            //A vector to record new directions of this robot; 
+    static int count_robots;                                         //How many robots in this place;
 public:
     Robot(double ,double ,double ,double ,double ,double, int);
 
@@ -37,11 +49,11 @@ public:
 
     double getY() const;
 
-    void change_location(double ,double);                               //change the robot location;
+    void change_location(double ,double);                         //change the robot location;
 
     double getTheta() const;                                      //get the robot direction
 
-    void change_theta(double);                                  //change the robot direction
+    void change_theta(double);                                    //change the robot direction
 
     int getCount() const;                                         //count the robot numbers
 
@@ -51,7 +63,7 @@ public:
 
     int getID() const;
 
-    void capture(int);                            //capture information from near object;
+    void capture(int);                                            //capture information from near object;
 
     void add_trace(Robot* ,Robot*);
 
